e56473109ecba6178df9712821c154b185ece7fa
Abinav Neyyar <abinav.neyyar@gmail.com>
2025-07-13 16:41:04 -0700

    Adds WSGI entry point for the application
    Sets up the main WSGI entry point using a factory pattern to
    create the Flask application.
    
    This allows the application to be run using a WSGI server,
    such as Gunicorn or uWSGI.
    It also includes a development-only run configuration with
    debug mode enabled.

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+"""
+Main application entry point for TutorConnect.
+"""
+from factory import create_app
+
+# Create the application
+app = create_app()
+
+# Run the application
+if __name__ == '__main__':
+    # Debug=True allows auto-reloading and provides detailed error pages
+    # Turn off debug mode in production!
+    app.run( port=5001)
